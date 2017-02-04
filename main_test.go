2e8d40b1e68ef05d882b7e97a31cf65e9a1db4d6
taroooyan <taroooyan@gmail.com>
2017-02-05 03:34:21 +0900

    change http test

--- main_test.go
+++ main_test.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"google.golang.org/appengine/aetest"
 	"testing"
 )
 
 func TestIsErrorStatus(t *testing.T) {
+	inst, err := aetest.NewInstance(nil)
+	if err != nil {
+		t.Fatalf("Failed to create instance: %v", err)
+	}
+	defer inst.Close()
+
 	// normal url
+	req1, err := inst.NewRequest("GET", "/888", nil)
+	if err != nil {
+		t.Fatalf("Failed to create req1: %v", err)
+	}
+
 	url := "http://www.adventar.org/calendars/888"
-	result := IsErrorStatus(url)
+	result := IsErrorStatus(url, req1)
 	if true == result {
 		t.Errorf("result is %v", result)
 		t.Fail()
 	}
 
 	// error url
+	req2, err := inst.NewRequest("GET", "/88", nil)
+	if err != nil {
+		t.Fatalf("Failed to create req2: %v", err)
+	}
+
 	url = "http://www.adventar.org/calendars/88"
-	result = IsErrorStatus(url)
+	result = IsErrorStatus(url, req2)
 	if false == result {
 		t.Errorf("result is %v", result)
 		t.Fail()
@@ -41,8 +58,19 @@ func TestIsErrorNumber(t *testing.T) {
 }
 
 func TestScraping(t *testing.T) {
+	inst, err := aetest.NewInstance(nil)
+	if err != nil {
+		t.Fatalf("Failed to create instance: %v", err)
+	}
+	defer inst.Close()
+
+	req, err := inst.NewRequest("GET", "/888", nil)
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+
 	url := "http://www.adventar.org/calendars/888"
-	result := Scraping(url)
+	result := Scraping(url, req)
 	title := "Aizu Advent Calendar 2015"
 	if result.Title != title {
 		t.Errorf("Return title is %s", result.Title)
